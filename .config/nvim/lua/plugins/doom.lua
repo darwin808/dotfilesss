14b61657a57ad7b0a660c2958102bf661ff44acc
Darwin ❤️❤️❤️ <71545960+darwin808@users.noreply.github.com>
2023-12-26 21:22:41 +0800

    feat: nvim (#1)
    Co-authored-by: darwin.apolinario <darwin.apolinario@gmail.com>

---
+++ .config/nvim/lua/plugins/doom.lua
@@ -0,0 +1,10 @@
+return {
+
+  { "romgrk/doom-one.vim" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "doom-one",
+    },
+  },
+}
---
+++ .config/nvim/lua/plugins/treeSitter.lua
@@ -0,0 +1,24 @@
+return {
+  { import = "lazyvim.plugins.extras.lang.typescript" },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+      },
+    },
+  },
+}
