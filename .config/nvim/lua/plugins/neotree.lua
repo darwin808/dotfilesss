91b3f3ecc0e8491c2a933510ee31be585775aa9b
Darwin ❤️❤️❤️ <71545960+darwin808@users.noreply.github.com>
2023-12-29 12:19:03 +0800

    feat: deps in neotree (#6)
    * feat: deps in neotree
    
    * feat: disable tre sitter ctx
    
    ---------
    
    Co-authored-by: darwin.apolinario <darwin.apolinario@gmail.com>

--- .config/nvim/lua/plugins/neotree.lua
+++ .config/nvim/lua/plugins/neotree.lua
@@ -2,6 +2,11 @@ return {
   "nvim-neo-tree/neo-tree.nvim",
   branch = "v3.x",
   cmd = "Neotree",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
   keys = {
     {
       "<leader>fe",
