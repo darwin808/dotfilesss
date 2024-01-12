9f67536b482bb3791249aa26ca612446b7c4c304
darwin.apolinario <darwin.apolinario@gmail.com>
2024-01-12 10:08:09 +0800

    feat: update

--- .config/nvim/lua/plugins/neotree.lua
+++ .config/nvim/lua/plugins/neotree.lua
@@ -2,11 +2,6 @@ return {
   "nvim-neo-tree/neo-tree.nvim",
   branch = "v3.x",
   cmd = "Neotree",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons",
-    "MunifTanjim/nui.nvim",
-  },
   keys = {
     {
       "<leader>fe",
@@ -42,14 +37,7 @@ return {
   deactivate = function()
     vim.cmd([[Neotree close]])
   end,
-  init = function()
-    if vim.fn.argc(-1) == 1 then
-      local stat = vim.loop.fs_stat(vim.fn.argv(0))
-      if stat and stat.type == "directory" then
-        require("neo-tree")
-      end
-    end
-  end,
+  init = function() end,
   opts = {
     sources = { "filesystem", "buffers", "git_status", "document_symbols" },
     open_files_do_not_replace_types = { "terminal", "Trouble", "trouble", "qf", "Outline" },
@@ -57,6 +45,13 @@ return {
       bind_to_cwd = false,
       follow_current_file = { enabled = true },
       use_libuv_file_watcher = true,
+      filtered_items = {
+        visible = true,
+        hide_dotfiles = false,
+        hide_gitignored = true,
+        hide_by_name = {},
+        never_show = { ".git" },
+      },
     },
     window = {
       position = "right",
